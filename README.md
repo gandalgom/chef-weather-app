45b21633c7b311872c8e813c65071457af7d4c07
Max Yoo <devbear74@gmail.com>
2022-06-23 17:51:51 +0900

    Geolocation 라이브러리 부착 및 현재 위치 수집기능 구현

--- README.md
+++ README.md
@@ -18,9 +18,18 @@
 * Android Studio Chipmunk (2021.2.1, patch 1)
 * Flutter 3.0.2
 
+### 사용 라이브러리 (2022.06 기준)
+
+* Geolocator 8.2.1 [링크](https://pub.dev/packages/geolocator)
+
 ### Git 이용시 장점
 
 git을 이용하여 코드를 내려받을 경우, 주요 업데이트 시점으로 reset 해가면서 진행 상황을 보는 것이 가능  
 최초 시작 시점의 경우 별도의 base 브랜치로 분리하여 두었으므로, checkout 하여 거기서부터 작업 가능  
 
 ```git checkout base```
+
+### Geolocator 사용시 Tip
+
+* 안드로이드 최신 버전에서는 위치 획득 전에 명시적으로 권한 획득 여부를 물어봐야 함. [공식 사이트 예제](https://pub.dev/packages/geolocator#example)대로 할 것
+* 안드로이드 에뮬레이터에서 현재 위치를 잡기 힘들 경우 내장된 구글 지도 앱을 실행해 주면 쉽게 잡을 수 있음.
--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -8,18 +9,63 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  String currentPosition = 'Where is my location?';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            textStyle: const TextStyle(color: Colors.white),
-          ),
-          child: const Text('Get my location'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                Position newPosition = await _determinePosition();
+                setState(() => currentPosition = newPosition.toString());
+              },
+              style: ElevatedButton.styleFrom(
+                textStyle: const TextStyle(color: Colors.white),
+              ),
+              child: const Text('Get my location'),
+            ),
+            const SizedBox(height: 40.0),
+            Text(
+              currentPosition,
+              style: const TextStyle(fontSize: 15.0),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+        'Location permissions are permanently denied, '
+        'we cannot request permissions.'
+      );
+    }
+
+    return await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high
     );
   }
 }
