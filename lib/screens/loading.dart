23482f06d45b9a1201bf86dc2536604b7c3c5d6b
Max Yoo <devbear74@gmail.com>
2022-06-24 13:43:32 +0900

    http 라이브러리 부착 및 샘플 날씨정보 수집 후 json parsing 기능 구현

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -11,6 +14,13 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
 
   String currentPosition = 'Where is my location?';
+  String currentWeather = "How's the weather?";
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,12 +46,21 @@ class _LoadingState extends State<Loading> {
             const SizedBox(height: 40.0),
             Text(
               currentPosition,
-              style: const TextStyle(fontSize: 15.0),
+              style: const TextStyle(fontSize: 16.0),
             ),
+            const SizedBox(height: 40.0),
+            Text(currentWeather),
           ],
         ),
       ),
     );
+  }
+
+  void resetScreenInfo() {
+    setState(() {
+      currentPosition = 'Where is my location?';
+      currentWeather = "How's the weather?";
+    });
   }
 
   Future<Position> _determinePosition() async {
@@ -71,5 +90,26 @@ class _LoadingState extends State<Loading> {
     return await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high
     );
+  }
+
+  void fetchData() async {
+    var url = Uri.parse('https://samples.openweathermap.org/data/2.5/'
+      'weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1');
+    http.Response response = await http.get(url);
+
+    final String resultData;
+
+    if (response.statusCode == 200) {
+      String jsonData = response.body;
+      var description = jsonDecode(jsonData)['weather'][0]['description'];
+      var wind = jsonDecode(jsonData)['wind']['speed'];
+      var id = jsonDecode(jsonData)['id'].toString();
+
+      resultData = 'description: $description\nwind speed: $wind\nid : $id';
+    } else {
+      resultData = 'Cannot collect weather info!';
+    }
+
+    setState(() => currentWeather = resultData);
   }
 }
